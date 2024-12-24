5dc4ec6ee72e39eeb23363cbef54b4f468877cd7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-12-24 18:05:03 +0530

    Update index.php

--- employees/index.php
+++ employees/index.php
@@ -5,15 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Management System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container mt-5">
         <h2 class="text-center">Manage Employees</h2>
 
-        <!-- Add User Form -->
+        <!-- Add Employee Form -->
         <div class="mt-4">
             <form action="create.php" method="post">
                 <div class="form-row">
@@ -80,7 +79,7 @@
                             </tr>";
                         }
                     } else {
-                        echo "<tr><td colspan='5' class='text-center>No Employees Found </td></tr>";
+                        echo "<tr><td colspan='5' class='text-center'>No Employees Found </td></tr>";
                     }
                     $conn->close();
                     ?>
