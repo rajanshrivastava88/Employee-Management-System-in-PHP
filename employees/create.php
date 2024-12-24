efc1218e37be2931d7da8ed9852d58b7efc846b7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-12-24 12:54:05 +0530

    Create create.php

---
+++ employees/create.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'db.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+
+    $sql = "INSERT INTO users (name, email, age) VALUES ('$name', '$email', $age)";
+
+    if($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+    }
+    else {
+        echo "Error: ".$sql. "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+
+?>
